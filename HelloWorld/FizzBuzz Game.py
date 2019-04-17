135fbd2722138fc9b3832980a013c6f181018a51
Yousuf Ali <yousuf-123@hotmail.co.uk>
2019-04-17 10:58:41 +0100

    updated work

--- HelloWorld/FizzBuzz Game.py
+++ HelloWorld/FizzBuzz Game.py
@@ -3,10 +3,16 @@ num2 = int(input("please enter your ending range: "))
 
 for i in range (num,num2):
     output = ""     # this will either be Fizz, or Buzz, or Fizzbuzz
-    if i % 3 == 0:
+    if i %3 == 0 and i %5 == 0:
+        output = "FizzBuzz"
+    elif i % 3 == 0:
         output = output + "Fizz"    # if i is divisible by 3 then output Fizz
-    if i % 5 == 0:
+    elif i % 5 == 0:
         output = output + "Buzz"    # if i is divisible by 5 then output Buzz
-    if i % 5 != 0 and i % 3 != 0:
+    else:
+        #i % 5 != 0 and i % 3 != 0:
         output = output + str(i)    #if not then output i (a number in the range)
-    print(output)
+    print(output) 
+
+#for num in range(100):
+#    print(num)
