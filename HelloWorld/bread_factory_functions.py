7644cbea46c0fe823bcfd60b18f5ccdc7f4f351f
Yousuf Ali <yousuf-123@hotmail.co.uk>
2019-04-18 16:06:35 +0100

    bread factory functions

--- HelloWorld/bread_factory_functions.py
+++ HelloWorld/bread_factory_functions.py
@@ -4,3 +4,22 @@ def dough(water,wheat):
         return "dough"
     else:
         return "blob"
+
+def bake(something):
+    if something == "dough":
+        return "this is bread"
+    else:
+        return "not bread"
+
+print(dough("water","wheat"))
+print(bake("dough"))
+
+
+def bread_factory(sub1,sub2):
+    output = dough(sub1,sub2)
+    return bake(output)
+
+bread_factory("water","flour")
+
+print(bread_factory("water","cement") == "not bread")
+print(bread_factory("water", "cement"))
